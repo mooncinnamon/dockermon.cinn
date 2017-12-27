8f9beb3c08b7a0d6181d3a627f298daf06ac72f6
Moon-Cinnamon <moon.pinnamon@gmail.com>
2017-12-27 16:02:12 +0900

    setting beat

--- monitoring/celery.py
+++ monitoring/celery.py
@@ -3,17 +3,18 @@ from __future__ import absolute_import
 import os
 
 from celery import Celery
+#from celery.signals import worker_shutdown
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monitoring.settings')
 
-from django.conf import settings
-
 # set the default Django settings module for the 'celery' program.
 
-app = Celery('monitoring',)  # 여기서 ggg는 task를 포함하는 app이름이 되겠습니다.
+app = Celery('monitoring')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
 
 
 @app.task(bind=True)
